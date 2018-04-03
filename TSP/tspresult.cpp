cf6dc2c613bc5861216b7c6c58d12ef775cabc86
Tony Eastwood <erlinmaquer@gmail.com>
2018-04-03 08:43:08 +0300

    delete queue from tspresult.cpp. add comments to some functions, start writing function that find short path by double replace method.

--- TSP/tspresult.cpp
+++ TSP/tspresult.cpp
@@ -8,13 +8,13 @@ TSPResult::TSPResult(QWidget *parent) :
     ui->setupUi(this);
 }
 
-void TSPResult::InitialInvolvePoints(bool (&involve_points)[QUANT_POINTS], int _quantity_points)
+void TSPResult::InitialInvolvePoints(bool (&involve_points)[QUANT_POINTS], int _quantity_points)    //appropriate all massive value 'false'
 {
     for(int i=0;i<_quantity_points;i++)
         involve_points[i]=false;
 }
 
-TSPResult::TSPResult(double (&_matrix_distance)[QUANT_POINTS][QUANT_POINTS], int _quantity_points):
+TSPResult::TSPResult(double (&_matrix_distance)[QUANT_POINTS][QUANT_POINTS], int _quantity_points): //Constructor with parametres
     QWidget(0),
     ui(new Ui::TSPResult)
 {
@@ -29,66 +29,82 @@ TSPResult::TSPResult(double (&_matrix_distance)[QUANT_POINTS][QUANT_POINTS], int
 
 }
 
-QString TSPResult::AlgorythmStartPath(double (&original_massive)[QUANT_POINTS][QUANT_POINTS], double (&result_matrix_distance)[QUANT_POINTS][QUANT_POINTS], int start_point, double &sum_path)
+QString TSPResult::AlgorythmStartPath(double original_massive[QUANT_POINTS][QUANT_POINTS], int start_point, double &sum_path)
+ //geedy algorythm search start short path
 {
-        QueueClear(CurrentShortPath);                   //clear queue
-        sum_path=0;
+
+        sum_path=0;                                     //start sum =0
         bool InvolvedPoints[QUANT_POINTS];                      //all points that we visit
         InitialInvolvePoints(InvolvedPoints, quantity_points);                   //set all value of massive eque to 'false'
         InvolvedPoints[start_point-1]=true;         //start point = true
-        for(int i=0;i<quantity_points;i++)
-            for(int j=0;j<quantity_points;j++)
-            result_matrix_distance[i][j]=original_massive[i][j];
 
-        QString path="";
-        path+=QString::number(start_point)+ " ";
-        int min_distance=_INFINITY;
-        int index=start_point-1;
+
+        QString path="";                                //will contain path like '1->5->7->1' that we return
+        path+=QString::number(start_point)+ " ";        //add start point in path
+        int min_distance=_INFINITY;                     //min_distance = infinity
+        int index=start_point-1;                        //index = start point
+        int k=0;                                //current index in CurrentShortPath massive
         while(true)
         {
             int i=index;
             min_distance=_INFINITY;
-            for(int j=0;j<quantity_points;j++)
+            for(int j=0;j<quantity_points;j++)              //find min path and check point as visited
             {
-                if(result_matrix_distance[i][j]<min_distance && InvolvedPoints[j]==false)
+                if(original_massive[i][j]<min_distance && InvolvedPoints[j]==false) //if path = min and point not visited
                 {
-                    index=j;
-                    min_distance=result_matrix_distance[i][j];
+                    index=j;                            //memorize index
+                    min_distance=original_massive[i][j];    //memorize min distance
                 }
             }
-            if (i==index)
-                break;
-            sum_path+=min_distance;
-            path+="->"+QString::number(index+1);
-            InvolvedPoints[index]=true;
-            CurrentShortPath.push(index);
+            if (i==index)                   //if all points visited
+                break;                      //exit from cycle and return QString
+            sum_path+=min_distance;                 //process path length
+            path+="->"+QString::number(index+1);        //write in path next point '1->5->current point'
+            InvolvedPoints[index]=true;             //check current point as visited
+            CurrentShortPath[k]=index;           //add current point in queue
+            k++;
         }
-        path+="->"+QString::number(start_point); //add start point at the and '1->....->1'
+        path+="->"+QString::number(start_point); //add start point at the and '1->....->1'      //add start point in the end
 
         return path;            //return path '1->3->5->1'
+}
+
+QString TSPResult::AlgotythmDoubleReplace(double original_massive[QUANT_POINTS][QUANT_POINTS], int start_point, double &sum_path)
+{
+    QString path="";
+    int int_path[QUANT_POINTS-1];
+    for(int i=0; i<quantity_points-1;i++)
+        int_path[i]=CurrentShortPath[i];
+
+    return path;
 }
 
 TSPResult::~TSPResult()
 {
     delete ui;
+
 }
 
-void TSPResult::on_buttStartPath_clicked()
+void TSPResult::on_buttStartPath_clicked()      //search start path via geedy algorythm
 {
 
     int start = ui->editStartPoint->text().toInt();         //point that will be start for out path                                         //all path convert from matrix distance to '1->2->5->7->1' etc.
-    double result_path[QUANT_POINTS][QUANT_POINTS];
     double path_length=0;
-    ui->labelPathStart->setText(AlgorythmStartPath(point_distance, result_path, start, path_length ));
+    ui->labelPathStart->setText(AlgorythmStartPath(point_distance, start, path_length ));
     //show short path in label '1->2...->1'
     ui->labelResultStart->setText(QString::number(path_length));    //show path length (sum all distance )
 
 }
 
-void TSPResult::QueueClear(std::queue<int> &q)
+
+
+
+
+void TSPResult::on_buttDoubleReplacement_clicked()
 {
-     std::queue<int> empty;
-       std::swap( q, empty );
+    int start = ui->editStartPoint->text().toInt();         //point that will be start for out path                                         //all path convert from matrix distance to '1->2->5->7->1' etc.
+    double path_length=0;
+    ui->labelPathStart->setText(AlgotythmDoubleReplace(point_distance, start, path_length ));
+    //show short path in label '1->2...->1'
+    ui->labelResultStart->setText(QString::number(path_length));    //show path length (sum all distance )
 }
-
-
--- TSP/tspresult.h
+++ TSP/tspresult.h
@@ -1,9 +1,8 @@
 #ifndef TSPRESULT_H
 #define TSPRESULT_H
-#define QUANT_POINTS 15            //max quantity of points that file can involve
+#define QUANT_POINTS 15            //max quantity of points that file can involve   (if points so many, app can crash )
 #define _INFINITY  1000000          //max distance between points
 #include <QWidget>
-#include <queue>
 namespace Ui {
 class TSPResult;
 }
@@ -14,17 +13,19 @@ class TSPResult : public QWidget
 
 public:
     explicit TSPResult(QWidget *parent = 0);
-    TSPResult(double (&)[QUANT_POINTS][QUANT_POINTS], int );
-    QString AlgorythmStartPath(double (&)[QUANT_POINTS][QUANT_POINTS], double (&)[QUANT_POINTS][QUANT_POINTS], int, double &);
-    //Original massive, massive that we return as refer, start position (point that will be start ), and length of path (sum) , type double
+    TSPResult(double (&)[QUANT_POINTS][QUANT_POINTS], int );            //constructor with parameters
+    QString AlgorythmStartPath(double [QUANT_POINTS][QUANT_POINTS], int, double &);  //geedy algorythm search start short path
+    QString AlgotythmDoubleReplace(double [QUANT_POINTS][QUANT_POINTS], int, double &); //algorythm local find, double replace
+    //Original massive, start position (point that will be start ), and length of path (sum) , type double
     ~TSPResult();
 
 private slots:
-    void on_buttStartPath_clicked();
+    void on_buttStartPath_clicked();            //search start path (geedy algorythm) and show result
+
+    void on_buttDoubleReplacement_clicked();
 
 private:
-    void QueueClear(std::queue<int> &q);
-    std::queue<int> CurrentShortPath;
+    int CurrentShortPath[QUANT_POINTS];                       //massive that contain path like '1 2 6 3 4' that search due to geedy algorythm
     QString ConvertFromPathToQString();                    //convert bool matrix to QString like '1->5->6->3->1' !!!!!!!!!!!!1
     double point_distance[QUANT_POINTS][QUANT_POINTS];    //matrix distance between points
     int quantity_points;                                   //quantity points in matrix
